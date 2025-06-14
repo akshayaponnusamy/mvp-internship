8e9c52d5228880f5986107ebf36b0291e76e73b5
Akshaya <71762307005@cit.edu.in>
2025-06-14 17:37:16 +0530

    document changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXX');
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
