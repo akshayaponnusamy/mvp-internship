f5dd3542dad6aa9e97a21a9757cdce467e4b5327
Akshaya <71762307005@cit.edu.in>
2025-06-14 18:54:03 +0530

    changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 // src/pages/_app.js
-import React from 'react';
 import '../styles/globals.css';
-import UploadFile from '../components/UploadFile'; // ✅ Updated path
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <h1>🎓 Student Grievance App</h1>
-      <UploadFile />
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
